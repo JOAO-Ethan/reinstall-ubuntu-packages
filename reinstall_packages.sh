d49a0d940503a82e87514fde3dddba1bffa9cb29
JOAO-Ethan <106932691+JOAO-Ethan@users.noreply.github.com>
2023-01-17 21:07:29 +0100

    Version 1.0 du script

---
+++ reinstall_packages.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "écriture du paquet..."
+sudo apt list --installed | sed  -e 's/^\(.*\)\/.*/sudo apt install \1/' | sed -e 's/\(En train de lister.\)/#!\/bin\/bash/' > ./setup.sh
+chmod 777 ./setup.sh
+echo "Pour réinstaller les paquets, veuillez lancer le script setup.sh"
